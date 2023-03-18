8e6c9ea921cbf83bf91615d58e62d37a7021bdac
Minde0727 <98126251+Minde0727@users.noreply.github.com>
2023-03-18 20:10:06 +0200

    Add files via upload
    Help puslapis

---
+++ ClientApp/src/components/HelpPage/HelpPage.js
@@ -0,0 +1,18 @@
+﻿import React from 'react';
+import './HelpPage.css';
+
+const HelpPage = () => {
+    return (
+        <div className="section">
+            Jei iškilo problemų, su mumis galite susisiekti:<br />
+            <p style="color:blue; text-decoration-line: underline;">
+                roklau1@ktu.lt<br />
+                minrus@ktu.lt<br />
+                ostmac@ktu.lt<br />
+                jusand3@ktu.lt<br />
+                zilkra@ktu.lt
+            </p>
+        </div>
+    )
+}
+export default HelpPage
