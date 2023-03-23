742c566d7ebea630196f7a2951cbd864fe39a897
Rokki-R <qvertas@gmail.com>
2023-03-23 19:56:05 +0200

    Added logout button under user profile section

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,7 +1,30 @@
-﻿import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, NavbarText, Button } from 'reactstrap';
+﻿import  { React, Component } from 'react';
+import { Collapse, Navbar, NavbarBrand, NavItem, NavLink, NavbarText, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { withRouter } from './withRouter';
+import toast from 'react-hot-toast';
 import './NavMenu.css';
+
+
+const testLogout = () => {
+    const requestOptions = {
+        method: "GET"
+    };
+    
+
+    fetch("api/login/logout", requestOptions)
+        .then(response => {
+            if (response.status === 200) { // 200 - Ok
+                toast('Logged out');
+            }
+            else if (response.status === 401) { // 401 - Unauthorized
+                alert('Already logged out');
+            }
+            else { // 500 - Internal server error
+                alert('Unexpected response, check console logs');
+            }
+        })
+}
 
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
@@ -10,10 +33,11 @@ export class NavMenu extends Component {
         super(props);
         this.state = {
             isClicked: false,
-            isLoggedIn: false // Assume the user is not logged in by default
+            isLoggedIn: false
         };
         this.handleClick = this.handleClick.bind(this);
     }
+
 
     handleClick() {
         this.setState({
@@ -34,6 +58,7 @@ export class NavMenu extends Component {
                 <ul className="no-bullets">
                     <li><Button tag={Link} to="/login" color="primary" onClick={this.handleClick} className="mb-2">Prisijungti</Button></li>
                     <li><Button tag={Link} to="/registration" color="primary" onClick={this.handleClick} className="mb-2">Registruotis</Button></li>
+                    <li><Button tag={Link} to="/login" color="primary" onClick={() => {testLogout(); this.handleClick()}} className="mb-2">Atsijungti</Button></li>
                 </ul>
             </Collapse>
         );
@@ -72,3 +97,4 @@ export class NavMenu extends Component {
         );
     }
 }
+export default withRouter(NavMenu)
