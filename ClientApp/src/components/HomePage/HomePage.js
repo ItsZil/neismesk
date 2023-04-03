9e2f6e3b7cbf688acdf986c404e4b8abeb689998
JustasAn <lamas123321@gmail.com>
2023-04-03 17:34:20 +0300

    Upgraded navigation bar so that it has a search bar and an item creation button.

--- ClientApp/src/components/HomePage/HomePage.js
+++ ClientApp/src/components/HomePage/HomePage.js
@@ -5,19 +5,6 @@ import './HomePage.css';
 function HomePage() {
     return (
         <div className="home">
-            <div class="give-container">
-                <button type="submit">Dovanoti</button>
-            </div>
-            <div class="search-container">
-                <input type="text" placeholder="Ieškoti..."/>
-                    <select>
-                        <option value="">Kategorijos</option>
-                        <option value="category1">Stambi buitinė technika</option>
-                        <option value="category2">Smulki buitinė techinka</option>
-                        <option value="category3">Kompiuteriai</option>
-                    </select>
-                <button type="submit">Ieškoti</button>
-            </div>
             <section className="product-list">
                 <h3>Naujausi prietaisų skelbimai</h3>
                 <ul className="ul">
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,7 +1,9 @@
 ﻿import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, NavbarText, Button } from 'reactstrap';
+import {
+    Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, NavbarText, Button, Nav, InputGroup, InputGroupAddon, Input, Dropdown, DropdownToggle, DropdownMenu, DropdownItem  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
+
 
 export class NavMenu extends Component {
     static displayName = NavMenu.name;
@@ -10,7 +12,9 @@ export class NavMenu extends Component {
         super(props);
         this.state = {
             isClicked: false,
-            isLoggedIn: false // Assume the user is not logged in by default
+            isLoggedIn: false, // Assume the user is not logged in by default
+            dropdownOpen: false,
+            selectedCategory: 'Kategorijos',
         };
         this.handleClick = this.handleClick.bind(this);
     }
@@ -19,9 +23,29 @@ export class NavMenu extends Component {
         this.setState({
             isClicked: !this.state.isClicked
         });
+
     }
 
+    selectCategory = category => {
+        this.setState({
+            selectedCategory: category,
+        });
+    };
+
+    toggleDropdown = () => {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen,
+        }));
+    };
+
+
     render() {
+        const maxCategoryLength = 15; // Maximum number of characters to display in dropdown toggle
+
+        let displayCategory = this.state.selectedCategory;
+        if (displayCategory.length > maxCategoryLength) {
+            displayCategory = displayCategory.substring(0, maxCategoryLength) + '...';
+        }
         const toolbar = this.state.isLoggedIn ? (
             <Collapse isOpen={this.state.isClicked} className="toolbar" style={{ flexDirection: 'column' }}>
                 <ul className="no-bullets">
@@ -45,7 +69,23 @@ export class NavMenu extends Component {
                         <NavbarBrand tag={Link} to="/">
                             <img alt="logo" src="./images/logo.png" />
                         </NavbarBrand>
-                        <NavbarText>Atiduok. Iškeisk. Laimėk</NavbarText>
+                        <div className="d-flex align-items-center">
+                        <div class="search-container">
+                        <Input type="search" placeholder="Ieškoti" style={{ width: '350px' }} />
+                        <Nav navbar>
+                            <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+                                        <DropdownToggle nav caret className="categories">{displayCategory}</DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem onClick={() => this.selectCategory('Telefonai')}>Telefonai</DropdownItem>
+                                    <DropdownItem onClick={() => this.selectCategory('Kompiuteriai')}>Kompiuteriai</DropdownItem>
+                                    <DropdownItem onClick={() => this.selectCategory('Stambi buitinė technika')}>Stambi buitinė technika</DropdownItem>
+                                </DropdownMenu>
+                            </Dropdown>
+                            </Nav>
+                        </div>    
+                            <Button className="buttonsearch" >Ieškoti</Button>
+                        </div>
+                        <Button className="buttongive">Dovanoti!</Button>
                         <NavItem className="profileContainer">
                             <img alt="profilis" src="./images/profile.png" onClick={this.handleClick} />
                             {toolbar}
