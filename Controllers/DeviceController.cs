288dd7b3dea97f91737123ccd0e77f1ad1e1d21b
JustasAn <lamas123321@gmail.com>
2023-04-05 19:27:33 +0300

    Now the main page takes the devices/items from our database and displays them.

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using neismesk.Controllers.UserAuthentication;
+using neismesk.Utilities;
+using neismesk.ViewModels;
+using System.Data;
+
+namespace neismesk.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DeviceController : ControllerBase
+    {
+        private readonly DatabaseAccess _database;
+
+        public DeviceController()
+        {
+            _database = new DatabaseAccess();
+        }
+
+        [HttpGet("getDevices")]
+        public async Task<IActionResult> GetDevices()
+        {
+
+            try
+            {
+                var devices = await _database.LoadData("SELECT * FROM ads");
+
+                if (devices == null)
+                {
+                    return BadRequest();
+                }
+
+                var result = (from DataRow dt in devices.Rows
+                              select new DeviceViewModel()
+                              {
+                                  Id = Convert.ToInt32(dt["id"]),
+                                  Name = dt["name"].ToString(),
+                                  Description = dt["description"].ToString()
+                              }).ToList();
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
