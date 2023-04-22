f84e959c2f9dfbc7ff9af0e3ba9e1f3c7a23225e
Ostinas <ostinasmaciulis@gmail.com>
2023-04-22 16:25:06 +0300

    Fixed merge errors

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -66,7 +66,6 @@ const AppRoutes = [
       path: '/skelbimas/naujas',
       element: <ItemCreationPage />
   },
-  }
 ];
 
 export default AppRoutes;
--- ClientApp/src/components/HomePage/HomePage.js
+++ ClientApp/src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 ﻿import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import toast, { Toaster } from 'react-hot-toast';
 import axios from 'axios';
 import './HomePage.css';
